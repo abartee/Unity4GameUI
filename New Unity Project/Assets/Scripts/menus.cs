86bad2b0b5363f706a56109257b12fc3372852b2
MSI\abart <abartee8@gmail.com>
2018-06-27 07:52:56 -0600

    project submission

---
+++ New Unity Project/Assets/Scripts/menus.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menus : MonoBehaviour {
+
+    public bool inGame = false;
+
+    public GameObject mainMenu;
+    public GameObject pauseMenu;
+    public GameObject optionMenu;
+    public GameObject inGamePauseButton;
+
+    public void Start()
+    {
+        inGame = false;
+        mainMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+        optionMenu.SetActive(false);
+    }
+
+    public void pause()
+    {
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+        optionMenu.SetActive(false);
+    }
+
+    public void options()
+    {
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        optionMenu.SetActive(true);
+    }
+
+    public void closeOptions()
+    {
+        optionMenu.SetActive(false);
+
+        if (inGame)
+        {
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            mainMenu.SetActive(true);
+        }
+    }
+
+    public void resume()
+    {
+        inGame = true;
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        optionMenu.SetActive(false);
+    }
+
+    public void quit()
+    {
+        inGame = false;
+        mainMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+        optionMenu.SetActive(false);
+    }
+}
